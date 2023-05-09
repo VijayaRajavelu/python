8d147d465b51785e9bc3c29b9d7d733145dfbd1a
VijayaRajavelu <vijaya.saravanan2007.gmail.com>
2023-05-09 21:03:42 +0100

    first commit

---
+++ primenumber.py
@@ -0,0 +1,17 @@
+#Find given number is prime or not.
+
+num = int(input("Enter a number: "))
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
